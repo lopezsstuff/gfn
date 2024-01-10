350bba8e5453a18752910d11904996ea6a69e6f4
lopezsstuff <135654910+lopezsstuff@users.noreply.github.com>
2024-01-09 19:42:08 -0500

    Update Desktop.bat

--- Desktop.bat
+++ Desktop.bat
@@ -1,6 +1,6 @@
 title Desktop Installer
 curl -O "https://cdn.discordapp.com/attachments/1146790169060585502/1185934241691873361/barb.zip?ex=65916a54&is=657ef554&hm=3152b97beac36488b1d278fc15acb00ac9179f818e23907b0654dcc3579e4173&"
-tar -xcf c:\users\kiosk\downloads\barb.zip
+tar -xf c:\users\kiosk\downloads\barb.zip
 taskkill /IM explorer.exe
 
-c:\users\kiosk\downloads\start.bin
+"c:\users\kiosk\downloads\start.bin"
